4316ba639a1687fb14ed35865bf69cbb4aecd894
hhgyu <hhgyu15@gmail.com>
2019-02-24 18:39:16 +0900

    add rotate

--- max7219.ts
+++ max7219.ts
@@ -139,12 +139,50 @@ namespace MAX7219_Matrix {
             for (let j = 0; j < _displayArray.length - 1; j++) {
                 _displayArray[j] = _displayArray[j + 1]
             }
+	
+            let displayArray: number[] = []
+            for (let i = 0; i < _displayArray.length; i++) {
+                displayArray.push(_displayArray[i])
+            }
+
+	    if(_rotate >= 1) {
+	        let rotate = _rotate;
+	        do {
+		    let tmpDisplayArray: number[] = []
+	            for (let i = 0; i < _displayArray.length; i++) {
+		        tmpDisplayArray.push(0)
+	            }
+
+	            for (let i = 0; i < _matrixNum + 2; i++) {
+		        for (let j = 0; j < 8; j++) {
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 7] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 7) : 0
+	    	            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 6] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 6) : 0
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 5] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 5) : 0
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 4] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 4) : 0
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 3] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 3) : 0
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 2] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 2) : 0
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 1] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 1) : 0
+		            tmpDisplayArray[(i * 8) + j] |= (displayArray[(i * 8) + 0] & (0x1 << (7 - j))) == (0x1 << (7 - j)) ? (0x1 << 0) : 0
+		        }
+	            }
+	    
+                    for (let i = 0; i < _displayArray.length; i++) {
+	                displayArray[i] = tmpDisplayArray[i]
+	            }
+		    rotate--;
+	        } while(rotate > 0);
+	    } else {
+                for (let i = 0; i < _displayArray.length; i++) {
+	            displayArray[i] = _displayArray[i]
+	        }
+	    }
+
             // write every 8 columns of display array (visible area) to each MAX7219s
             let matrixCountdown = _matrixNum - 1
             for (let j = 8; j < _displayArray.length - 8; j += 8) {
                 if (matrixCountdown < 0) break
                 for (let k = j; k < j + 8; k++) {
-                    _registerForOne(_DIGIT[k - j], _displayArray[k], matrixCountdown)
+                    _registerForOne(_DIGIT[k - j], displayArray[k], matrixCountdown)
                 }
                 matrixCountdown -= 1
             }
